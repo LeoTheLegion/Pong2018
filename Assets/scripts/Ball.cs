25dc54a571bfb7b50f559891f772c315edd59dd7
mmena2017 <mmena2017@gmail.com>
2018-05-21 01:58:00 -0400

    Updated Ball inspector

--- Assets/scripts/Ball.cs
+++ Assets/scripts/Ball.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour {
+
+	public int speed = 100;
+	public Vector2 starting_direction = Vector2.up + Vector2.left;
 	
 	Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
-		rb.AddForce ((Vector2.up + Vector2.left) * 100);
+		rb.AddForce (starting_direction* speed);
 	}
 	
 	// Update is called once per frame
